f928640bbe3ee8b5d573c80322ee68a695a4dda4
WuYaoke <wuykjiu@163.com>
2023-07-18 17:22:54 +0800

    perf: 新增国际化字段

--- src/app/[locale]/compoments/EmptyState.tsx
+++ src/app/[locale]/compoments/EmptyState.tsx
@@ -3,31 +3,33 @@
 import { useRouter } from "next/navigation";
 import Heading from "./Heading";
 import Button from "./Button";
+import { useTranslations } from "next-intl";
 
 interface EmptyProps {
   title?: string;
-  subtitle?: string;
+  subTitle?: string;
   showReset?: boolean;
 }
 
 const EmptyState: React.FC<EmptyProps> = ({
-  title = "No exact matches",
-  subtitle = "Try changing or removing some of you filters",
+  title = "title",
+  subTitle = "subTitle",
   showReset,
 }) => {
+  const t = useTranslations("EmptyPage");
   const router = useRouter();
   return (
     <div className="h-[60vh] flex flex-col justify-center items-center gap-2">
       <Heading
         center
-        title={title}
-        subtitle={subtitle}
+        title={t(title)}
+        subtitle={t(subTitle)}
       />
       <div className="w-48 mt-4">
         {showReset && (
           <Button
             outline
-            label="Remove all filters"
+            label={t("clearFilters")}
             onClick={() => router.push("/")}
           />
         )}
